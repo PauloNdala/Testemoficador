02595fefbb66e4f775c5466ce17e90e54a03f87a
Paulo Ndumba Ndala <87448044+PauloNdala@users.noreply.github.com>
2022-03-07 13:45:22 +0000

    Adiconei arquivo via upload

---
+++ Testemoficador/src/wakandatech/ndala.java
@@ -0,0 +1,16 @@
+package wakandatech;
+
+import Aual1.Modificadores.paulo;
+
+public class ndala {
+    
+	public static void main(String[] args) {
+	  paulo f = new paulo();
+	  f.nome = "paulo";
+	  f.salario();
+	  f.idSalario = 12;
+	  System.out.println(f.nome);
+	  System.out.println(f.salario());
+	}
+
+}
